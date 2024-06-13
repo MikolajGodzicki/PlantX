683899297a9c466a6009564ba4cd4e747d7f872a
Mikołaj Godzicki <mikolajgodzicki@gmail.com>
2024-06-14 00:34:55 +0200

    Updated

---
+++ PlantX/Controls/MenuButtonControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media;
+
+namespace PlantX.Controls
+{
+	public class MenuButtonControl : RadioButton {
+		static MenuButtonControl() {
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(MenuButtonControl), new FrameworkPropertyMetadata(typeof(MenuButtonControl)));
+		}
+
+		public static readonly DependencyProperty IconSourceProperty =
+			DependencyProperty.Register("IconSource", typeof(ImageSource), typeof(MenuButtonControl), new PropertyMetadata(default(ImageSource)));
+
+		public ImageSource IconSource {
+			get { return (ImageSource)GetValue(IconSourceProperty); }
+			set { SetValue(IconSourceProperty, value); }
+		}
+	}
+}
