32fbe81b8594f23cc4b4e2554a0880edbe19421a
Mikołaj Godzicki <mikolajgodzicki@gmail.com>
2024-06-14 21:02:28 +0200

    Update

---
+++ PlantX/MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantX.MVVM.ViewModels {
+	internal class MainViewModel : ViewModelBase {
+		private PlantsCreatorViewModel _plantsCreatorVM { get; set; }
+
+		private object _currentView;
+
+		public object CurrentView {
+			get { return _currentView; }
+			set { _currentView = value;
+				OnPropertyChanged();
+			}
+		}
+
+
+		public MainViewModel()
+        {
+			_plantsCreatorVM = new PlantsCreatorViewModel();
+			CurrentView = _plantsCreatorVM;
+		}
+    }
+}
---
+++ PlantX/MVVM/ViewModels/PlantsCreatorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantX.MVVM.ViewModels {
+	internal class PlantsCreatorViewModel : ViewModelBase {
+        public PlantsCreatorViewModel()
+        {
+            
+        }
+    }
+}
